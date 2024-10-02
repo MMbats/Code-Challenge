f57884b50f51c557d10c490944cf055811239ea0
MMbats <petermbatia657@gmail.com>
2024-10-02 14:36:26 +0300

    Added salary

--- net salary calculator.js
+++ net salary calculator.js
@@ -4,13 +4,13 @@ function calculatePayee(basicSalary){
     let tax = 0
     if (basicSalary <= 24000){
 tax = basicSalary *0.1;
-    }else if(basicSalary >24000 && basicSalary <=32333){
+    } else if (basicSalary >24000 && basicSalary <=32333){
         tax = 2400 +(basicSalary - 24000) *0.25;
-    }else if(basicSalary > 32333 && basicSalary>= 500000){
+    } else if (basicSalary > 32333 && basicSalary>= 500000){
         tax = 4800 +(basicSalary - 32333)*0.30
-    } else if(basicsalary >500000 && basicSalary<=800000){
+    } else if (basicsalary >500000 && basicSalary<=800000){
         tax = 6800 + (basicSalary - 500000)*0.32;
-    }else if(basicSalary > 800000){
+    } else if (basicSalary > 800000){
         tax = 11325 + (basicSalary - 800000) *0.35;
     }
         return tax;
@@ -19,44 +19,45 @@ tax = basicSalary *0.1;
 function calculateNHIF(basicSalary){
     if (basicSalary >= 59999){
         return 150;
-    }else if(basicSalary >6000 && basicSalary <=7999){
+    } else if (basicSalary >6000 && basicSalary <=7999){
         return 300;
-    }else if(basicSalary >8000 && basicSalary <=11999){
+    } else if (basicSalary >8000 && basicSalary <=11999){
         return 400;
-    }else if(basicSalary >12000 && basicSalary <=14999){
+    } else if (basicSalary >12000 && basicSalary <=14999){
         return 500;
-    }else if(basicSalary >15000 && basicSalary <=19999){
+    } else if (basicSalary >15000 && basicSalary <=19999){
         return 600;
-    }else if(basicSalary >20000 && basicSalary <=24999){
+    } else if (basicSalary >20000 && basicSalary <=24999){
         return 750;
-    }else if(basicSalary >25000 && basicSalary <=29999){
+    }else if (basicSalary >25000 && basicSalary <=29999){
         return 850;
-    }else if(basicSalary >30000 && basicSalary <=34999){
+    } else if (basicSalary >30000 && basicSalary <=34999){
         return 900;
-    }else if(basicSalary >40000 && basicSalary <=44999){
+    } else if (basicSalary >40000 && basicSalary <=44999){
         return 1000;
-    }else if(basicSalary >45000 && basicSalary <=49999){
+    } else if (basicSalary >45000 && basicSalary <=49999){
         return 1100;
-    }else if(basicSalary >50000 && basicSalary <=59999){
+    } else if (basicSalary >50000 && basicSalary <=59999){
         return 1200;
-    }else if(basicSalary >60000 && basicSalary <=69999){
+    } else if (basicSalary >60000 && basicSalary <=69999){
         return 1300;
-    }else if(basicSalary >70000 && basicSalary <=79999){
+    } else if (basicSalary >70000 && basicSalary <=79999){
         return 1400;
-    }else if(basicSalary >80000 && basicSalary <=89999){
+    } else if (basicSalary >80000 && basicSalary <=89999){
         return 1500;
-    }else if(basicSalary >90000 && basicSalary <=99999){
+    } else if (basicSalary >90000 && basicSalary <=99999){
         return 1600;
-    }else if(basicSalary =>100000 ){
+    } else if (basicSalary =>100000 ){
         return 1700;
     }
+
     function calculateNSSF(amount){
         let nssfContribution = 0;
-        if (amount <= 7000){0
+        if (amount <= 7000){
             nssfContribution = amount *0.06
-        }else if (amount >= 7001 && amount <= 36000){
+        } else if (amount >= 7001 && amount <= 36000){
             nssfContribution = 420
-        }else if{
+        } else if {
             nssfContribution = 420 +(amount - 7000) *0.06
         }
         return nssfContribution;
@@ -66,7 +67,7 @@ function calculateNHIF(basicSalary){
         return basicSalary *0.015;
     }
 function calculateNetSalary(basicSalary,benefits){
-    const grossSalary =basicSalary + benefits;
+    const grossSalary = basicSalary + benefits;
     const payee = calculatePayee(basicSalary);
     const nhif =calculateNHIF(basicSalary);
     const nssf =calculateNSSF(basicSalary);
@@ -81,6 +82,7 @@ return{
     housinglevy,
     netSalary
 };
+
 }
 function main(){
     const readline = require('readline').createInterface({
@@ -89,11 +91,11 @@ function main(){
     })
     readline.questions('enter the basic salary:'(answer)=>{
         const basicSalary= parseFloat(answer)
-        readline.questions('enter the benefits:',(answer)=>){
+        readline.questions('enter the benefits:', (answer)=>){
             const benefits = parseFloat (answer)
 
-            const { grossSalary,payee,nhif,nssf,housinglevy, netSalary} =calculateNetSalary(basicSalary,benefits):
-            // Displat the results
+            const { grossSalary ,payee nhif , nssf , housingLevy , netsalary } = calculateNetSalary(basicSalary , benefits);
+            // Display the results 
             console.log("\n--- Salary Summary ---"); 
             console.log(`Gross Salary: KES ${grossSalary}`); 
             console.log(`PAYE Tax: KES ${payee}`); 
